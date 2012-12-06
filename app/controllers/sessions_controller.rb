8144e3b905debbc089f3130fb3025d481b62b578
Gregory Brown <gregory.t.brown@gmail.com>
2012-12-06 13:15:19 -0500

    Force authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,14 @@
 class SessionsController < ApplicationController
+  skip_before_filter :authorize_user, :only => :create
+
   def create
     Person.find_or_create_by_uid(auth_hash["uid"])
     session[:identity] = auth_hash["uid"]
 
     redirect "/"
   end
+
+  private
 
   def auth_hash
     hash = request.env['omniauth.auth']
