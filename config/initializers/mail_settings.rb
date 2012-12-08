bffdf374c23fef2dfc3ce08194c2423b701037c2
Gregory Brown <gregory.t.brown@gmail.com>
2012-12-08 09:28:16 -0500

    Add mail config

---
+++ config/initializers/mail_settings.rb
@@ -0,0 +1,9 @@
+ctionMailer::Base.smtp_settings = {
+  :address              => "smtp.sendgrid.net",
+  :port                 => 587,
+  :domain               => "practicingruby.com",
+  :user_name            => ENV["MAIL_USER"],
+  :password             => ENV["MAIL_PASSWORD"},
+  :authentication       => "plain",
+  :enable_starttls_auto => true
+}
